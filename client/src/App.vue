202ef024d47a0657322f0779dc9eddc43df78301
Benjamin-Gruber <83592290+Benjamin-Gruber@users.noreply.github.com>
2021-10-01 14:23:31 +0200

    öffnen

---
+++ client/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container fluid class="black">
+        <img src="../public/logo.png" alt="Logo" width="25%">
+        <p class="text-h2 text-center"></p>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data: () => ({}),
+};
+</script>
