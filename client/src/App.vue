9a410d357c83c975466f532aeb83d40f699ae06b
Benjamin-Gruber <83592290+Benjamin-Gruber@users.noreply.github.com>
2021-10-02 10:19:07 +0200

    Übergabe
    Holen der Daten vom Server via Axios und Übergabe an CarCards.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,18 +1,40 @@
 <template>
   <v-app>
-    <LogoBar />   
+    <LogoBar />  
+    <CarCards :cars="cars"/> 
   </v-app>
 </template>
 
 <script>
 import LogoBar from '@/components/LogoBar.vue';
+import CarCards from '@/components/CarCards.vue';
+import axios from "axios";
 
 export default {
   name: 'App',
-  data: () => ({}),
+  data() {
+    return{
+      cars: [],
+    }
+  },
 
   components: {
     LogoBar,
+    CarCards,
+  },
+
+  methods: {
+    async getCars() {
+      try {
+        const {data} = await axios({
+          url: "http://localhost:3000/cars",
+          method: "GET",
+        });
+        this.cars = data;
+      } catch (error) {
+        console.error(error);
+      }
+    }
   },
 };
 </script>
